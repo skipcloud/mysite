9f9b9a4778016a489b9354498744a9dbbfb6b656
Skip Gibson <skipcloudgibson@gmail.com>
2022-04-03 17:10:15 +0100

    header: highlight active page in navbar

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,15 +1,40 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
 import styles from './header.module.css'
 
 export default function Header() {
+  const router = useRouter()
   return (
     <header>
       <nav className={styles.navbar}>
         <ul>
-          <li>home</li>
-          <li>blog</li>
-          <li>music</li>
+          {linkItems(router)}
         </ul>
       </nav>
     </header>
   )
+} 
+
+function linkItems(router) {
+  const links = ['home', 'blog', 'music'];
+
+  return links.map(value => {
+    let path = `/${value}`;
+    if (value === 'home') {
+      path = '/'
+    }
+
+    console.log(`/${path}`)
+    return (
+      <li 
+        key={value} 
+        className={router.pathname === path ? styles.activePage : ''}>
+        <Link href={path}>
+          {value} 
+        </Link>
+      </li>
+    )    
+  })
 }
