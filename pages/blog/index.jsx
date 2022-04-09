d92ae9d318767deed880e2dd3662a1025607f142
Skip Gibson <skipcloudgibson@gmail.com>
2022-04-09 09:30:27 +0100

    blog: add basic list of posts

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,8 +1,67 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
 import * as React from 'react'
+import Link from 'next/link'
 import Layout from '../../components/layout'
 
-export default function Blog() {
+const postsPath = path.join(process.cwd(), 'pages/posts')
+
+export default function Blog({ postData }) {
   return (
-    <Layout />
+    <Layout>
+      { posts(postData) }
+    </Layout>
   )
+}
+
+function posts(posts) {
+  const links = posts.map(post => {
+    return (
+      <li key={post.id}>
+        <Link href={`/posts/${post.id}`} >{ post.id }</Link>
+      </li>
+    )
+  })
+
+  return (
+    <ul>{ links }</ul>
+  )
+}
+
+function getPostData() {
+  const filePaths = fs.readdirSync(postsPath)
+
+  const content = filePaths.map(function(filePath) {
+    if (filePath === '') {
+      return
+    }
+    const file = fs.readFileSync(`${postsPath}/${filePath}`, {encoding: 'utf8'})
+    const content = matter(file)
+
+    // this is the original buffer, we already get the
+    // string from content.content so just delete it to 
+    // reduce prop size
+    delete content.orig
+    const id = filePath.replace(/.md$/, '')
+
+    console.log(content.orig)
+    return {
+      id
+    }
+  })
+
+  return content.filter(post => {
+    return post !== undefined
+  })
+}
+
+export async function getStaticProps() {
+  const postData = getPostData()
+
+  return {
+    props: {
+      postData
+    }
+  }
 }
