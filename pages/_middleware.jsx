d8e9369b4a94269383ca754ee8953f031885f08d
Skip Gibson <skipcloudgibson@gmail.com>
2022-04-09 09:10:49 +0100

    middleware: remove punctuation from old urls

--- pages/_middleware.jsx
+++ pages/_middleware.jsx
@@ -3,13 +3,18 @@ import { NextResponse } from "next/server";
 // My old site (RIP) used /YYYY/MM/DD/<title> as the path so
 // to ensure the old links continue to work just gently rewrite
 // the URL to point to the new /posts/<title> path
+//
+// Some old posts have commas, colons, and question marks in the
+// title so remove them too
 export function middleware(request) {
   // match '/YYYY/MM/DD/<post-title>'
   const oldPostPath = /^\/\d{4}(\/\d{2}){2}\/(\w+)/
-  const { pathname } = request.nextUrl
-  
+  let { pathname } = request.nextUrl
+
+  // remove problematic punctuation from title
+  pathname = pathname.replaceAll(/[,:?]/g, '')
+
   if (oldPostPath.test(pathname)) {
-    // const postName = pathname.match(oldPostPath)[2]
     const url = request.nextUrl.clone()
     url.pathname = '/posts/hello-world'
     const resp = NextResponse.redirect(url, 301)
