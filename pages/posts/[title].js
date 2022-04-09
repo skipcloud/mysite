4bec62a1aa468db82d979b1eb3e869816123b23c
Skip Gibson <skipcloudgibson@gmail.com>
2022-04-09 10:17:40 +0100

    posts: statically generate blog posts

--- pages/posts/[title].js
+++ pages/posts/[title].js
@@ -1,6 +1,38 @@
-import { useRouter } from "next/router";
+import Layout from '../../components/layout'
+import matter from 'gray-matter'
+import path from 'path'
+import fs from 'fs'
 
-export default function Post() {
-  const { query } = useRouter()
-  return <h1>{ query.title }</h1>
+const markdownPath = path.join(process.cwd(), 'posts_markdown')
+
+export default function Post({ post }) {
+  return (
+    <Layout>{ post }</Layout>
+  )
+}
+
+export async function getStaticProps({ params }) {
+  const file = fs.readFileSync(`${markdownPath}/${params.title}.md`, 'utf8')
+  const { content } = matter(file)
+  return {
+    props: {
+      post: content
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  const posts = fs.readdirSync(markdownPath)
+  const paths = posts.map(post => {
+    return {
+      params: {
+        title: post.replace(/.md$/, '')
+      }
+    }
+  })
+
+  return {
+    paths,
+    fallback: false
+  }
 }
